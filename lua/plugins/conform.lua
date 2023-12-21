464e3727fa46325f24f5d08f2424c54a3fc3bd57
Gabriel Miranda <gabrielmio16@gmail.com>
2023-12-21 16:47:41 -0300

    Added conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettier" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				timeout_ms = 500,
+			})
+		end)
+	end,
+}
