40a519f57b6991426847152ee8f712ec5d0c5ca0
gabriel <gabrielmio16@gmail.com>
2024-06-22 22:23:29 -0300

    feat: add formatting toggle

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,10 +13,35 @@ return {
 				typescriptreact = { { "prettierd", "prettier" } },
 				rust = { "rustfmt" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 		})
+
+		vim.api.nvim_create_user_command("FormatToggle", function(args)
+			if args.buffer then
+				vim.b.disable_autoformat = not vim.b.disable_autoformat
+				if vim.b.disable_autoformat then
+					print("autoformat-on-save disabled for this buffer")
+				else
+					print("autoformat-on-save enabled for this buffer")
+				end
+			else
+				vim.g.disable_autoformat = not vim.g.disable_autoformat
+				if vim.g.disable_autoformat then
+					print("autoformat-on-save disabled globally")
+				else
+					print("autoformat-on-save enabled globally")
+				end
+			end
+		end, {
+			desc = "Toggle autoformat-on-save",
+			bang = true,
+		})
+
+		vim.keymap.set("n", "<leader>uf", "<cmd>FormatToggle<cr>", { silent = true, desc = "Toggle Format" })
 	end,
 }
